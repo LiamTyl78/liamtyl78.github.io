e6d0b2a0c3f354dc5bd35c25956ae84cc94ad115
Liam Tyler <tylerl68@students.rowan.edu>
2025-03-07 00:35:06 -0500

    added blur effect to titlebar and improved formatting on projects page.

--- scripts/header.js
+++ scripts/header.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const headerContainer = document.getElementById("header-container");
+
+
     fetch("/pages/header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-container").innerHTML = data;
+            headerContainer.innerHTML = data;
             highlightActiveLink();
+            const header = headerContainer.querySelector("header");
+            if (header) {
+                window.addEventListener("scroll", function(){
+                    if(window.scrollY > 50){
+                        header.classList.add("small-header")
+                    }
+                    else{
+                        header.classList.remove("small-header")
+                    }
+                })
+            }            
         })
         .catch(error => console.error("Error loading the header:", error));
 });
